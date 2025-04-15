d994c86ec6150e7fa5387e3b8843b3461469a0f1
JeRoHnII <166115134+JeRoHnII@users.noreply.github.com>
2025-04-15 10:53:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -174,7 +174,5 @@ if __name__ == "__main__":
     static_dir.mkdir(exist_ok=True)
 
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))  # Render will set this dynamically
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
-
-    
